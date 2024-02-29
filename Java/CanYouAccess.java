b41eebf96c7d00b7af7bc40ff5893646f1905a72
myus3rnam3159 <le3akmailg@gmail.com>
2024-02-29 16:29:48 +0700

    access private class in java

--- Java/CanYouAccess.java
+++ Java/CanYouAccess.java
@@ -2,6 +2,7 @@ package Java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.security.Permission;
 
 public class CanYouAccess {
     public static void main(String[] args) throws Exception {
