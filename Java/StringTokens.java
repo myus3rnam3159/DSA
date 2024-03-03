c7f7e2ebd0680364257808bf2c524051362f4947
myus3rnam3159 <le3akmailg@gmail.com>
2024-03-03 09:30:02 +0700

    string tokens

---
+++ Java/StringTokens.java
@@ -0,0 +1,21 @@
+package Java;
+
+import java.util.Scanner;
+
+public class StringTokens {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        scan.close();
+        s = s.trim();
+        if (s.length() == 0) {
+            System.out.println(0);
+            return;
+        }
+        String[] words = s.split("[^a-zA-Z]+");
+        System.out.println(words.length);
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
