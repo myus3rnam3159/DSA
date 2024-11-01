9990df914517f7ce5775291ea5a4f2c44a9858fe
myus3rnam3159 <le3akmailg@gmail.com>
2024-11-01 18:44:06 +0700

    finish removing element

---
+++ Java/LeetCode/ArrayString/RemoveElement.java
@@ -0,0 +1,15 @@
+package ArrayString;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.length; ++i) {
+            if(nums[i] != val) {
+                nums[k] = nums[i];
+                k += 1;
+            }
+        }
+
+        return k;
+    }
+}
