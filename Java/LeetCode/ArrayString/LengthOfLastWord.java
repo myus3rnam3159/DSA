4d1b0b200628a5238361ca4bd5bbb7c288724c41
myus3rnam3159 <le3akmailg@gmail.com>
2024-11-22 12:06:31 +0700

    finish length-of-last-work challenge optimized

--- Java/LeetCode/ArrayString/LengthOfLastWord.java
+++ Java/LeetCode/ArrayString/LengthOfLastWord.java
@@ -2,21 +2,12 @@ package ArrayString;
 
 public class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
-        int start = s.length() - 1;
-        int end = start;
-        while(end >= 0){
-            if(s.charAt(end) != ' '){
-                
-            }
-            else{
-                if(end + 1 < s.length() && s.charAt(end + 1) != ' ') break;
-                end -= 1;
-                start = end;
-                continue;
-            }
+        int i = s.length();
+        int length = 0;
 
-            end -= 1;
-        }
-        return start - end;
+        while(s.charAt(--i) == ' ') {}
+        while(i >= 0 && s.charAt(i--) != ' ') {++length;}
+
+        return length;
     }
 }
