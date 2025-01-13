a30d62c3d44d68694e4d44236b28e4ea53d046ee
myus3rnam3159 <le3akmailg@gmail.com>
2025-01-13 08:50:02 +0700

    longest substring with non repeat letters

---
+++ Java/LeetCode/SlidingWindow/LongestSubStringNonRepeat.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubStringNonRepeat {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+
+        int l = 0;
+        int res = 0;
+
+        for(int r = 0; r < s.length(); ++r){
+            while(set.contains(s.charAt(r))){
+                set.remove(s.charAt(l));
+                ++l;
+            }
+            set.add(s.charAt(r));
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
