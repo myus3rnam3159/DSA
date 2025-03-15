46bcbd884a11138f28e9784f452d69886809be09
myus3rnam3159 <le3akmailg@gmail.com>
2025-03-15 10:58:52 +0700

    finish single number

---
+++ Java/LeetCode/BitManipulation/SingleNumber.java
@@ -0,0 +1,13 @@
+package BitManipulation;
+
+public class SingleNumber {
+    public int singleNumber(int [] nums) {
+        int result = 0;
+
+        for(int num: nums){
+            result ^= num;
+        }
+
+        return result;
+    }
+}
